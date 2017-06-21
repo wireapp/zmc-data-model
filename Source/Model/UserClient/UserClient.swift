7efc7bf745560674a19411abf7ad146294d12319
Gerasimenko Mike <mike@gerasimenko.me>
2017-06-21 11:23:08 +0200

    Revert fetch assert

--- Source/Model/UserClient/UserClient.swift
+++ Source/Model/UserClient/UserClient.swift
@@ -141,7 +141,9 @@ public class UserClient: ZMManagedObject, UserClientType {
         
         let existingClients = user.clients.filter({$0.remoteIdentifier == remoteIdentifier})
         
-        assert(existingClients.count <= 1, "Detected duplicate clients: \(existingClients.map({ $0.remoteIdentifier! }))")
+        if existingClients.count > 1 {
+            zmLog.error("Detected duplicate clients: \(existingClients.map({ $0.remoteIdentifier! }))")
+        }
         
         guard let client = existingClients.first
         else {
