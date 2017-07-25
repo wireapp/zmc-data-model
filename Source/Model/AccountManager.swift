7acbc82f05587df69ab305d19a5b149a41359286
Silvan Dähn <silvandaehn@me.com>
2017-07-25 14:28:59 +0200

    Update comment

--- Source/Model/AccountManager.swift
+++ Source/Model/AccountManager.swift
@@ -66,7 +66,7 @@ public final class AccountManager: NSObject {
         updateAccounts()
     }
 
-    /// Adds and selects an account to the manager and persists it.
+    /// Adds an account to the mananger and immediately and selects it.
     /// - parameter account: The account to add and select.
     public func addAndSelect(_ account: Account) {
         add(account)
