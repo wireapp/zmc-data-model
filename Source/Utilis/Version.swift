7e3913eb45f100942482709dbcb49866129a47ad
Silvan Dähn <silvandaehn@me.com>
2018-03-22 16:15:02 +0100

    Update tests

--- Source/Utilis/Version.swift
+++ Source/Utilis/Version.swift
@@ -1,9 +1,19 @@
 //
-//  Version.swift
-//  WireDataModel
+// Wire
+// Copyright (C) 2018 Wire Swiss GmbH
 //
-//  Created by Zeta on 22.03.18.
-//  Copyright © 2018 Wire Swiss GmbH. All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 import Foundation
@@ -50,6 +60,10 @@ import Foundation
         return .orderedSame
     }
     
+    public override func isEqual(_ object: Any?) -> Bool {
+        guard let other = object as? Version else { return false }
+        return other == self
+    }
     
     public override var description: String {
         return arrayRepresentation.map { "\($0)" }.joined(separator: ".")
