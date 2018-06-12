a7ed03be4023244bb8bf7905caa741a94bfc1aae
Vytis Šibonis <vytis@wire.com>
2018-06-12 14:37:28 +0200

    Fix tests compilation

--- Tests/Source/Model/Observer/MessageObserverTests.swift
+++ Tests/Source/Model/Observer/MessageObserverTests.swift
@@ -135,7 +135,7 @@ class MessageObserverTests : NotificationDispatcherTestBase {
         // given
         let clientMessage = ZMClientMessage(nonce: UUID.create(), managedObjectContext: uiMOC)
         let nonce = UUID.create()
-        clientMessage.add(ZMGenericMessage.message(text: name!, nonce: nonce).data())
+        clientMessage.add(ZMGenericMessage.message(text: name, nonce: nonce).data())
         let preview = ZMLinkPreview.linkPreview(
             withOriginalURL: "www.example.com",
             permanentURL: "www.example.com/permanent",
@@ -144,7 +144,7 @@ class MessageObserverTests : NotificationDispatcherTestBase {
             summary: "summary",
             imageAsset: nil
         )
-        let updateGenericMessage = ZMGenericMessage.message(text: name!, linkPreview: preview, nonce: nonce)
+        let updateGenericMessage = ZMGenericMessage.message(text: name, linkPreview: preview, nonce: nonce)
         uiMOC.saveOrRollback()
         
         // when
