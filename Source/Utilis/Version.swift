71b83d97bc9da84aa0eacd08e3734db263c40dbe
Silvan Dähn <silvandaehn@me.com>
2018-06-13 10:18:53 +0200

    Expose more methods using @objc

--- Source/Utilis/Version.swift
+++ Source/Utilis/Version.swift
@@ -18,10 +18,10 @@
 
 import Foundation
 
-@objc(ZMVersion) @objcMembers final public class Version: NSObject, Comparable {
+@objc(ZMVersion) final public class Version: NSObject, Comparable {
     
-    public private(set) var versionString: String
-    public private(set) var arrayRepresentation: [Int]
+    @objc public private(set) var versionString: String
+    @objc public private(set) var arrayRepresentation: [Int]
     
     @objc(initWithVersionString:)
     public init(string: String) {
