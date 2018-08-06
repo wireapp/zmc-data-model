2794bc2cbd553e6f58a3930ab5e278b207de93bc
Alexis Aubry <alexis.aubry@wire.com>
2018-08-06 11:20:00 +0200

    Add non-mutable user validators for better Swift import

--- Source/Public/ZMUser.h
+++ Source/Public/ZMUser.h
@@ -125,4 +125,10 @@ extern NSString * _Nonnull const ZMPersistedClientIdKey;
 + (BOOL)validatePassword:(NSString * __nullable * __nullable)ioPassword error:(NSError * __nullable * __nullable)outError;
 + (BOOL)validatePhoneVerificationCode:(NSString * __nullable * __nullable)ioVerificationCode error:(NSError * __nullable * __nullable)outError;
 
++ (BOOL)isValidName:(NSString *)name;
++ (BOOL)isValidEmailAddress:(NSString *)emailAddress;
++ (BOOL)isValidPassword:(NSString *)password;
++ (BOOL)isValidPhoneNumber:(NSString *)phoneNumber;
++ (BOOL)isValidPhoneVerificationCode:(NSString *)phoneVerificationCode;
+
 @end
