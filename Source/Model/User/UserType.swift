18028c927ea45b368789041eea245a4d34d7288d
Nicola Giancecchi <nicola@wire.com>
2019-02-18 09:40:19 +0100

    Added managedByWire to UserType protocol + ZMSearchUser implementation

--- Source/Model/User/UserType.swift
+++ Source/Model/User/UserType.swift
@@ -87,5 +87,8 @@ public protocol UserType: NSObjectProtocol {
     /// You should stop from observing the searchUser and start observing the user from there on
     func connect(message: String)
     
+    /// Determines whether the user profile is managed by Wire or other services (SCIM)
+    var managedByWire: Bool { get }
+    
 }
 
