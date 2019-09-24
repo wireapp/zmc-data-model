4a36536f581df0055e9599b3b057ab8d8f742910
Jacob Persson <jacob.persson@gmail.com>
2019-09-24 11:43:29 +0200

    Fix conversation directory did change not firing (#783)
    * fix conversationDirectoryDidChange not fired issue
    
    * observers all lists

--- Source/ConversationList/ConversationDirectory.swift
+++ Source/ConversationList/ConversationDirectory.swift
@@ -73,8 +73,15 @@ extension ZMConversationListDirectory: ConversationDirectoryType {
     
     public func addObserver(_ observer: ConversationDirectoryObserver) -> Any {
         let observerProxy = ConversationListObserverProxy(observer: observer, directory: self)
+
+        var tokens:[Any] = []
+        [archivedConversations, unarchivedConversations, pendingConnectionConversations, oneToOneConversations, groupConversations].forEach() {
+            let token = ConversationListChangeInfo.add(observer: observerProxy, for: $0, managedObjectContext: managedObjectContext)
+            tokens.append(token)
+        }
+
         let token = ConversationListChangeInfo.add(observer: observerProxy, managedObjectContext: managedObjectContext)
-        return [token, observerProxy]
+        return tokens + [token, observerProxy]
     }
     
 }
