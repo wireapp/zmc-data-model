98126adcae48e5fedb106c415c0b8fe3c3f046a2
John Nguyen <polyxo@protonmail.com>
2020-04-17 11:37:06 +0200

    [Fix] Broadcast recipients (#960)
    * Fix off by one count
    
    * Fix known team user calculation
    
    * Fix tests not running

--- Source/Model/User/ZMUser+Availability.swift
+++ Source/Model/User/ZMUser+Availability.swift
@@ -86,7 +86,7 @@ extension ZMUser {
             .prefix(remainingSlots)
 
         recipients.formUnion(teamMembers)
-        remainingSlots -= recipients.count
+        remainingSlots = maxCount - recipients.count
 
         guard remainingSlots > 0 else { return recipients }
 
@@ -120,13 +120,13 @@ extension ZMUser {
 
     static func knownTeamUsers(in context: NSManagedObjectContext) -> Set<ZMUser> {
         let connectedPredicate = ZMUser.predicateForUsers(withConnectionStatuses: [ZMConnectionStatus.accepted.rawValue])
-        let teamUserPredicate = NSPredicate(format: "(%K != NULL)", #keyPath(ZMUser.teamIdentifier))
-        let connectedAndTeamUserPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [connectedPredicate, teamUserPredicate])
-
         let request = NSFetchRequest<ZMUser>(entityName: ZMUser.entityName())
-        request.predicate = connectedAndTeamUserPredicate
+        request.predicate = connectedPredicate
 
-        return Set(context.fetchOrAssert(request: request))
+        let connections = Set(context.fetchOrAssert(request: request))
+        let selfUser = ZMUser.selfUser(in: context)
+        let result = connections.filter { $0.hasTeam && !$0.isOnSameTeam(otherUser: selfUser) }
+        return Set(result)
     }
 
     @objc public var availability : Availability {
