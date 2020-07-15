124d17046039ffba221db30c52060a816ccbbb45
marcomaddy1982 <30315526+marcomaddy1982@users.noreply.github.com>
2020-07-15 17:33:00 +0200

    [Feature]: Avoid Digital Signature Multiple Requests (#1001)
    * - added one more state that represent when the digital signature request is performing
    
    * - clean up
    
    * - clean up
    
    * Update test
    
    Co-authored-by: Jacob Persson <jacob.persson@gmail.com>

--- Source/Model/Message/File/SignatureStatus.swift
+++ Source/Model/Message/File/SignatureStatus.swift
@@ -85,8 +85,9 @@ public final class SignatureStatus: NSObject {
     }
     
     public func retrieveSignature() {
-        state = .waitingForSignature
-        RequestAvailableNotification.notifyNewRequestsAvailable(nil)
+         guard case .waitingForCodeVerification = state else { return }
+         state = .waitingForSignature
+         RequestAvailableNotification.notifyNewRequestsAvailable(nil)
     }
     
     public func didReceiveConsentURL(_ url: URL?) {
