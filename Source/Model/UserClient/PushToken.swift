57b19c0556969e4d9c74647175b07151d3a50f89
KaterinaWire <katerina@wire.com>
2020-09-08 08:52:27 +0300

    Modify Push Token

--- Source/Model/UserClient/PushToken.swift
+++ Source/Model/UserClient/PushToken.swift
@@ -18,10 +18,72 @@
 
 import Foundation
 
+public enum PushTokenType: Int, Codable {
+    case standard, voip
+
+    public var transportType: String {
+        switch self {
+            case .standard: return "APNS"
+            case .voip: return "APNS_VOIP"
+        }
+    }
+}
+
+public struct PushTokenMetadata {
+    let isSandbox: Bool
+    
+    /*!
+     @brief There are 4 different application identifiers which map to each of the bundle id's used
+     @discussion
+     com.wearezeta.zclient.ios-development (dev) - <b>com.wire.dev.ent</b>
+     
+     com.wearezeta.zclient.ios-internal (internal) - <b>com.wire.int.ent</b>
+     
+     com.wearezeta.zclient-alpha - <b>com.wire.ent</b>
+     
+     com.wearezeta.zclient.ios (app store) - <b>com.wire</b>
+     
+     @sa https://github.com/zinfra/backend-wiki/wiki/Native-Push-Notifications
+     */
+    let appIdentifier: String
+
+    /*!
+     @brief There are 4 transport types which depend on the token type and the environment
+     @discussion <b>APNS</b> -> ZMAPNSTypeNormal (deprecated)
+     
+     <b>APNS_VOIP</b> -> ZMAPNSTypeVoIP
+     
+     <b>APNS_SANDBOX</b> -> ZMAPNSTypeNormal + Sandbox environment (deprecated)
+     
+     <b>APNS_VOIP_SANDBOX</b> -> ZMAPNSTypeVoIP + Sandbox environment
+     
+     The non-VoIP types are deprecated at the moment.
+     
+     @sa https://github.com/zinfra/backend-wiki/wiki/Native-Push-Notifications
+     */
+    
+    var tokenType: PushTokenType
+    var transportType: String {
+        return isSandbox ? (tokenType.transportType + "_SANDBOX") : tokenType.transportType
+    }
+    
+    public static func current(for tokenType: PushTokenType) -> PushTokenMetadata {
+        let appId = Bundle.main.bundleIdentifier ?? ""
+        let buildType = BuildType.init(bundleID: appId)
+        
+        let isSandbox = ZMMobileProvisionParser().apsEnvironment == .sandbox
+        let appIdentifier = buildType.certificateName
+        
+        let metadata = PushTokenMetadata(isSandbox: isSandbox, appIdentifier: appIdentifier, tokenType: tokenType)
+        return metadata
+    }
+}
+
 public struct PushToken: Equatable, Codable {
     public let deviceToken: Data
     public let appIdentifier: String
     public let transportType: String
+    public let type: PushTokenType
     public var isRegistered: Bool
     public var isMarkedForDeletion: Bool = false
     public var isMarkedForDownload: Bool = false
@@ -29,8 +91,15 @@ public struct PushToken: Equatable, Codable {
 
 extension PushToken {
 
-    public init(deviceToken: Data, appIdentifier: String, transportType: String, isRegistered: Bool) {
-        self.init(deviceToken: deviceToken, appIdentifier: appIdentifier, transportType: transportType, isRegistered: isRegistered, isMarkedForDeletion: false, isMarkedForDownload: false)
+    public init(deviceToken: Data, tokenType: PushTokenType, isRegistered: Bool = false) {
+        let metadata = PushTokenMetadata.current(for: tokenType)
+        self.init(deviceToken: deviceToken,
+                  appIdentifier: metadata.appIdentifier,
+                  transportType: metadata.transportType,
+                  type: tokenType,
+                  isRegistered: isRegistered,
+                  isMarkedForDeletion: false,
+                  isMarkedForDownload: false)
     }
 
     public var deviceTokenString: String {
