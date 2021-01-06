1cc16f3fa115b741263b5aad00b572d73dad0c21
Jacob Persson <jacob.persson@gmail.com>
2021-01-06 11:49:02 +0100

    Expose senderClientID on senderClientID

--- Source/Public/ZMMessage.h
+++ Source/Public/ZMMessage.h
@@ -134,6 +134,7 @@ typedef NS_ENUM(int16_t, ZMSystemMessageType) {
 @property (nonatomic) BOOL needsUpdatingUsers;
 @property (nonatomic) NSTimeInterval duration;
 @property (nonatomic) NSNumber * _Nullable decryptionErrorCode; // Only filled for ZMSystemMessageTypeDecryptionFailed
+@property (nonatomic) NSString * _Nullable senderClientID; // Only filled for ZMSystemMessageTypeDecryptionFailed
 /**
   Only filled for .performedCall & .missedCall
  */
