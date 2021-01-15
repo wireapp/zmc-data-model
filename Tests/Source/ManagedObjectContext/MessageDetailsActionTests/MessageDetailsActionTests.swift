7157d4eb239cb03210bcfeb59003196e2f01a717
bill chan <billypchan@gmail.com>
2021-01-15 21:05:49 +0100

    fixed testThatDetailsAreAvailableInTeamGroup_Receipts

--- Tests/Source/ManagedObjectContext/MessageDetailsActionTests/MessageDetailsActionTests.swift
+++ Tests/Source/ManagedObjectContext/MessageDetailsActionTests/MessageDetailsActionTests.swift
@@ -24,12 +24,17 @@ final class MessageDetailsActionTests: ZMConversationTestsBase {
     private var selfUserInTeam: Bool = false
     var team: Team?
     var teamMember: Member?
+    var otherUser: ZMUser!
     
     override func setUp() {
         super.setUp()
+
+        otherUser = ZMUser.insertNewObject(in: uiMOC)
+        otherUser.remoteIdentifier = UUID()
     }
     
     override func tearDown() {
+        otherUser = nil
         super.tearDown()
     }
 
@@ -58,12 +63,12 @@ final class MessageDetailsActionTests: ZMConversationTestsBase {
         }
     }
     
-    /*func testThatDetailsAreAvailableInTeamGroup_Receipts() {
+    func testThatDetailsAreAvailableInTeamGroup_Receipts() {
         withGroupMessage(belongsToTeam: false, teamGroup: true) { message in
             XCTAssertTrue(message.areMessageDetailsAvailable(selfUser: self.selfUser))
             XCTAssertTrue(message.areReadReceiptsDetailsAvailable(selfUser: self.selfUser))
         }
-    }*/
+    }
     
     // MARK: - Messages Sent by Other User
 /*
@@ -105,7 +110,9 @@ final class MessageDetailsActionTests: ZMConversationTestsBase {
 
     // MARK: - Helpers
 
-    private func withGroupMessage(belongsToTeam: Bool, teamGroup: Bool, _ block: @escaping (ZMConversationMessage) -> Void) {
+    private func withGroupMessage(belongsToTeam: Bool,
+                                  teamGroup: Bool,
+                                  _ block: @escaping (ZMConversationMessage) -> Void) {
         let context = belongsToTeam ? teamTest : nonTeamTest
 
         context {
@@ -116,8 +123,7 @@ final class MessageDetailsActionTests: ZMConversationTestsBase {
             let message = MockConversationMessage()
             
             message.senderUser = self.selfUser//MockUserType.createSelfUser(name: "Alice")
-//            message.conversation = teamGroup ? self.createTeamGroupConversation() : self.createGroupConversation()
-            message.conversation = self.createGroupConversation()
+            message.conversation = teamGroup ? self.createTeamGroupConversation() : self.createGroupConversation()
             let textMessageData = MockTextMessageData()
             textMessageData.messageText = "blah"
             message.backingTextMessageData = textMessageData
@@ -137,9 +143,20 @@ final class MessageDetailsActionTests: ZMConversationTestsBase {
         }
     }
     
+    func createTeamGroupConversation() -> ZMConversation {
+        return createTeamGroupConversation(moc: uiMOC, otherUser: otherUser, selfUser: selfUser)
+    }
+
+    func createTeamGroupConversation(moc: NSManagedObjectContext,
+                                            otherUser: ZMUser,
+                                            selfUser: ZMUser) -> ZMConversation {
+        let conversation = createGroupConversation(moc: moc, otherUser: otherUser, selfUser: selfUser)
+        conversation.teamRemoteIdentifier = UUID.create()
+        conversation.userDefinedName = "Group conversation"
+        return conversation
+    }
+
     func createGroupConversation() -> ZMConversation {
-        let otherUser = ZMUser.insertNewObject(in: uiMOC)
-        otherUser.remoteIdentifier = UUID()
 //        otherUser.name = "Bruno"
 //        otherUser.setHandle("bruno")
 //        otherUser.accentColorValue = .brightOrange
@@ -207,122 +224,6 @@ final class MessageDetailsActionTests: ZMConversationTestsBase {
         teamMember!.setTeamRole(.member)
     }
 }
-
-/*
-final class MockMessage: NSObject, ZMConversationMessage {
-    /// Unique identifier for the message
-    var nonce: UUID?
-    
-    /// The user who sent the message (internal)
-    @available(*, deprecated, message: "Use `senderUser` instead")
-    var sender: ZMUser?
-    
-    /// The user who sent the message
-    var senderUser: UserType?
-    
-    /// The timestamp as received by the server
-    var serverTimestamp: Date?
-    
-    /// The conversation this message belongs to
-    var conversation: ZMConversation?
-    
-    /// The current delivery state of this message. It makes sense only for
-    /// messages sent from this device. In any other case, it will be
-    /// ZMDeliveryStateDelivered
-    var deliveryState: ZMDeliveryState = .delivered
-
-    /// True if the message has been successfully sent to the server
-    var isSent: Bool = false
-    
-    /// List of recipients who have read the message.
-    var readReceipts: [ReadReceipt] = []
-    
-    /// Whether the message expects read confirmations.
-    var needsReadConfirmation: Bool = false
-    
-    /// The textMessageData of the message which also contains potential link previews. If the message has no text, it will be nil
-    var textMessageData : ZMTextMessageData?
-    
-    /// The image data associated with the message. If the message has no image, it will be nil
-    var imageMessageData: ZMImageMessageData?
-    
-    /// The system message data associated with the message. If the message is not a system message data associated, it will be nil
-    var systemMessageData: ZMSystemMessageData?
-    
-    /// The knock message data associated with the message. If the message is not a knock, it will be nil
-    var knockMessageData: ZMKnockMessageData?
-    
-    /// The file transfer data associated with the message. If the message is not the file transfer, it will be nil
-    var fileMessageData: ZMFileMessageData?
-    
-    /// The location message data associated with the message. If the message is not a location message, it will be nil
-    var locationMessageData: LocationMessageData?
-    
-    var usersReaction : Dictionary<String, [ZMUser]> = Dictionary()
-    
-    /// In case this message failed to deliver, this will resend it
-    func resend() {
-        
-    }
-    
-    /// tell whether or not the message can be deleted
-    var canBeDeleted : Bool = false
-    
-    /// True if the message has been deleted
-    var hasBeenDeleted : Bool = false
-    
-    var updatedAt : Date?
-    
-    /// Starts the "self destruction" timer if all conditions are met
-    /// It checks internally if the message is ephemeral, if sender is the other user and if there is already an existing timer
-    /// Returns YES if a timer was started by the message call
-    func startSelfDestructionIfNeeded() -> Bool {
-        return false
-    }
-    
-    /// Returns true if the message is ephemeral
-    var isEphemeral : Bool = false
-    
-    /// If the message is ephemeral, it returns a fixed timeout
-    /// Otherwise it returns -1
-    /// Override this method in subclasses if needed
-    var deletionTimeout : TimeInterval = 0
-    
-    /// Returns true if the message is an ephemeral message that was sent by the selfUser and the obfuscation timer already fired
-    /// At this point the genericMessage content is already cleared. You should receive a notification that the content was cleared
-    var isObfuscated : Bool = false
-    
-    /// Returns the date when a ephemeral message will be destructed or `nil` if th message is not ephemeral
-    var destructionDate: Date?
-    
-    /// Returns whether this is a message that caused the security level of the conversation to degrade in this session (since the
-    /// app was restarted)
-    var causedSecurityLevelDegradation : Bool = false
-    
-    /// Marks the message as the last unread message in the conversation, moving the unread mark exactly before this
-    /// message.
-    func markAsUnread() {
-        
-    }
-    
-    /// Checks if the message can be marked unread
-    var canBeMarkedUnread: Bool = false
-    
-    /// The replies quoting this message.
-    var replies: Set<ZMMessage> = Set()
-    
-    /// An in-memory identifier for tracking the message during its life cycle.
-    var objectIdentifier: String = ""
-    
-    /// The links attached to the message.
-    var linkAttachments: [LinkAttachment]?
-    
-    /// Used to trigger link attachments update for this message.
-    var needsLinkAttachmentsUpdate: Bool = false
-    
-    var isSilenced: Bool = false
-
-}*/
 
 extension ZMConversation {
     
