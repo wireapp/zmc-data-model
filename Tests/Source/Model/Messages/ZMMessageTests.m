8406b4441fae6852b9404450805cef213522792a
Bill Chan <billypchan@gmail.com>
2021-03-02 19:01:03 +0100

    Update Tests/Source/Model/Messages/ZMMessageTests.m
    Co-authored-by: John Nguyen <polyxo@protonmail.com>

--- Tests/Source/Model/Messages/ZMMessageTests.m
+++ Tests/Source/Model/Messages/ZMMessageTests.m
@@ -1153,7 +1153,7 @@ NSUInteger const ZMClientMessageByteSizeExternalThreshold = 128000;
     // then
     XCTAssertNotNil(imageData);
     if (@available(iOS 14, *)) {
-        ///TODO: check why 1 btye is removed from jpegData?
+        // TODO:  [Bill] check why 1 btye is removed from jpegData?
         XCTAssertEqual(imageData.length, jpegData.length + 1);
     } else {
         XCTAssertEqual(imageData.length, jpegData.length);
@@ -1632,5 +1632,4 @@ NSUInteger const ZMClientMessageByteSizeExternalThreshold = 128000;
 }
 
 @end
-
 
