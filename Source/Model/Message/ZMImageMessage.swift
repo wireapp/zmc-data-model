75e282b0fd971ce70cc9e3bc80310c613a7136f9
bill <billypchan@gmail.com>
2021-05-28 12:31:03 +0200

    convert requestFileDownload

---
+++ Source/Model/Message/ZMImageMessage.swift
@@ -0,0 +1,31 @@
+//
+//  ZMImageMessage.swift
+//  WireDataModel
+//
+//  Created by Bill, Yiu Por Chan on 28.05.21.
+//  Copyright © 2021 Wire Swiss GmbH. All rights reserved.
+//
+
+import Foundation
+
+extension ZMImageMessage {
+    func requestFileDownload() {
+        // V2
+
+        // objects with temp ID on the UI must just have been inserted so no need to download
+        if objectID.isTemporaryID {
+            return
+        }
+
+        let moc = managedObjectContext?.zm_userInterface
+
+        if let moc = moc {
+            let note = NotificationInContext(
+                name: ZMAssetClientMessage.imageDownloadNotificationName,
+                context: moc.notificationContext,
+                object: objectID,
+                userInfo: nil)
+            note.post()
+        }
+    }
+}
