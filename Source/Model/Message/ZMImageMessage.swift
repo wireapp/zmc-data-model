b12c5452cfe27915f68585adb8532b25b6c099ae
Bill Chan <billypchan@gmail.com>
2021-06-22 15:49:55 +0200

    clean up requestFileDownload

--- Source/Model/Message/ZMImageMessage.swift
+++ Source/Model/Message/ZMImageMessage.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 extension ZMImageMessage {
+    /// Request the download of the image if not already present.
+    /// The download will be executed asynchronously. The caller can be notified by observing the message window.
+    /// This method can safely be called multiple times, even if the content is already available locally
     public func requestFileDownload() {
         // V2
 
@@ -17,9 +20,7 @@ extension ZMImageMessage {
             return
         }
 
-        let moc = managedObjectContext?.zm_userInterface
-
-        if let moc = moc {
+        if let moc = managedObjectContext?.zm_userInterface {
             let note = NotificationInContext(
                 name: ZMAssetClientMessage.imageDownloadNotificationName,
                 context: moc.notificationContext,
